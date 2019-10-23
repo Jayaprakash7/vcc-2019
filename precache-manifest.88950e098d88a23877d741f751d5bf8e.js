7c0da40de81e0b5ee454634fbaa3ad1e97c63be0
Jayaprakash7 <jayaprakash7995@gmail.com>
2019-10-24 03:39:08 +0530

    Updates

---
+++ precache-manifest.88950e098d88a23877d741f751d5bf8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ddd16fc6fc21af8ad237a8955665081",
+    "url": "/vcc-2019/index.html"
+  },
+  {
+    "revision": "f39ba1f82a1828eefb11",
+    "url": "/vcc-2019/static/css/main.d11100d0.chunk.css"
+  },
+  {
+    "revision": "ed5e6f724633a1aa93ce",
+    "url": "/vcc-2019/static/js/2.1fd9834a.chunk.js"
+  },
+  {
+    "revision": "f39ba1f82a1828eefb11",
+    "url": "/vcc-2019/static/js/main.45445314.chunk.js"
+  },
+  {
+    "revision": "775d8bf9f255fea36670",
+    "url": "/vcc-2019/static/js/runtime-main.c565da70.js"
+  },
+  {
+    "revision": "8f6da69ebd2615c74bc2aedb77f31046",
+    "url": "/vcc-2019/static/media/sportsday.8f6da69e.png"
+  }
+]);
